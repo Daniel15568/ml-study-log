e45bd7a951cea0fb0355c0683c0d5fd191b5ef97
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2024-08-09 23:22:31 +0100

    app.py
    Simplified the max_num function

--- Python Full Course 4hrs/app.py
+++ Python Full Course 4hrs/app.py
@@ -107,12 +107,7 @@ print(cube(3))
 
 # if statements & comparision
 def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num3 and num2 >= num1:
-        return num2
-    else:
-        return num3
+    return max(num1, num2, num3)
 
 print(max_num(4,2,10))
 
